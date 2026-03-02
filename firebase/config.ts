01f80d2aea9a2f2d36d75b052e060a51e22fa299
kumar774 <62069126+kumar774@users.noreply.github.com>
2026-03-02 12:50:11 +0530

    Update config.ts

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, doc, updateDoc, arrayUnion } from "firebase/firestore";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDt8jX6BVWa2cdY66zYBlSXP_3LiV8l3QQ",
@@ -16,36 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const messaging = getMessaging(app);
-
-export const VAPID_KEY = "BAdbh57jKtC3gMA69BcIAGMFu4j80l3ll5lFrQ5pPa-RQSGj3b9cSp1cM7jZf453FlVO0VeOGmfJXzfZzickG1c";
-
-export const requestForToken = async (restaurantId: string) => {
-  try {
-    let registration;
-    if ('serviceWorker' in navigator) {
-      registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
-    }
-    const currentToken = await getToken(messaging, { 
-      vapidKey: VAPID_KEY,
-      serviceWorkerRegistration: registration
-    });
-    if (currentToken) {
-      console.log('FCM Token:', currentToken);
-      // Save token to Firestore
-      const restaurantRef = doc(db, 'restaurants', restaurantId);
-      await updateDoc(restaurantRef, {
-        fcmTokens: arrayUnion(currentToken)
-      });
-      return currentToken;
-    } else {
-      console.log('No registration token available. Request permission to generate one.');
-      return null;
-    }
-  } catch (err) {
-    console.log('An error occurred while retrieving token. ', err);
-    return null;
-  }
-};
-
-export { onMessage };
+export const storage = getStorage(app);
